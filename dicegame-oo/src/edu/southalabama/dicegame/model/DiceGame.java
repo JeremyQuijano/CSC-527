27d42c611ae98061348ce261e6b5d5dfd89c947e
School <School@mehnovo.attlocal.net>
2025-01-27 20:42:52 -0600

    Finished classes and methods for Player, DiceGame, and Die.

--- dicegame-oo/src/edu/southalabama/dicegame/model/DiceGame.java
+++ dicegame-oo/src/edu/southalabama/dicegame/model/DiceGame.java
@@ -1,12 +1,54 @@
 package edu.southalabama.dicegame.model;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 
+
 public class DiceGame {
 	
-
+	private Die die1;
+	private Die die2;
+	static int numSides = 6;
 	
+	public DiceGame() {
+		this.die1 = new Die(numSides);
+		this.die2 = new Die(numSides);
+	}
+	
+	public void play() throws IOException {
+		die1.roll(numSides);
+		die1.getFaceValue();
+		die2.roll(numSides);
+		die2.getFaceValue();
+	}
 
+	public List<Integer> getDieValues() {
+		List<Integer> values = new ArrayList<Integer>();
+		values.add(die1.getFaceValue());
+		values.add(die2.getFaceValue());
+		
+		return values;
+	}
+	
+	public boolean getOutcome() {
+		boolean outcome;
+		
+		if (getResult() == 7 || getResult() == 12) {
+			outcome = true;
+		}
+		else {
+			outcome = false;
+		}
+		
+		return outcome;
+	}
+	
+	public int getResult() {
+		int result = die1.getFaceValue() + die2.getFaceValue();
+		
+		return result;
+	}
+	
 }
--- dicegame-oo/src/edu/southalabama/dicegame/model/Die.java
+++ dicegame-oo/src/edu/southalabama/dicegame/model/Die.java
@@ -1,7 +1,27 @@
 package edu.southalabama.dicegame.model;
 
+import java.util.Random;
+
 public class Die {
-
-
 	
+	private int faceValue;
+	private static int numSides;
+	
+	Die(int sides) {
+		this.faceValue = 0;
+		Die.numSides = sides;
+	}
+	
+	public int getFaceValue() {
+		return faceValue;
+	}
+	
+	public void roll(int numSides) {
+		
+		Random random = new Random();
+		
+		int randInt = random.nextInt(numSides) + 1;
+		faceValue = randInt;
+	}
+
 }
